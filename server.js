4433040f2db67b1fd7d686e11d84db437814a77e
Ben Flynn <flynn0087@gmail.com>
2020-12-16 18:10:09 -0600

    server creation and port selection

--- server.js
+++ server.js
@@ -2,3 +2,7 @@
 const express = require("express");
 const path = require("path");
 
+//sets up the express app and initial port, process.env will allow heroku's default
+const app = express();
+const PORT = process.env.PORT || 3000;
+
