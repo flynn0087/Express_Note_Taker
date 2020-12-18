8003d15e5b8e4e99b28659b862ba49b6f81f1c49
Ben Flynn <flynn0087@gmail.com>
2020-12-18 17:54:52 -0600

    add the path and another dependency

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 //dependencies for the server functionality
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 //sets up the express app and initial port, process.env will allow heroku's default
 const app = express();
 const PORT = process.env.PORT || 3000;
+const initialDir = path.join(__dirname, "/public")
+
+//this creates the absoulte path and allows serving of files
+const initialDir = path.join(__dirname, "/public");
+app.use(express.static());
 
 //this allows the express app to parse the data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//this creates the absoulte path
+const initialDir = path.join(__dirname, "/public");
+app.use(express.static());
 
 //the listener that starts the server
 app.listen(PORT, function() {
