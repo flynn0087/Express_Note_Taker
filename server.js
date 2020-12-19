be345966df2592b7fe7fea432e8b0afe10432a11
Ben Flynn <flynn0087@gmail.com>
2020-12-18 22:13:35 -0600

    removed modules, removed dependency, re-ordered routes

--- server.js
+++ server.js
@@ -2,44 +2,38 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const uuid = require("uuid")
 
 //sets up the express app and initial port, process.env will allow heroku's default
 const app = express();
 const PORT = process.env.PORT || 3000;
-const initialDir = path.join(__dirname, "/public")
 
 //this creates the absoulte path and allows serving of files
 const initialDir = path.join(__dirname, "/public");
-app.use(express.static());
+app.use(express.static("public"));
 
 //this allows the express app to parse the data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//these are the routes needed 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(initialDir, "notes.hmtl"));
+//this is an html route
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(initialDir, "notes.html"));
 });
 
-app.get("*", function(reg, res) {
+//these are the API routes
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+app.get("/api/notes/:id", (req, res) => {
+    let allNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(allNotes[Number(req.params.id)]);
+});
+
+//this was another html route, but due to wildcard symbol needed to be after the other routes
+app.get("*", function(req, res) {
     res.sendFile(path.join(initialDir, "index.html"));
 });
-
-app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));    
-});
-
-app.get("/api/notes/:id", function(req, res) {
-    let saved = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    res.JSON(saved[Number(req.param.id)]);
-});
-
-
-
-
-
-
 
 //the listener that starts the server
 app.listen(PORT, function() {
